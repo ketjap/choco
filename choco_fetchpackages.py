a407a38fd75a2b26d00c4c65f4581d5e10700c05
Sander Siemonsma <shrikes_number_0a@icloud.com>
2024-07-06 19:06:41 +0200

    include arguments, multiple file creation

--- choco_fetchpackages.py
+++ choco_fetchpackages.py
@@ -5,95 +5,114 @@ import json
 import sys
 import datetime
 import argparse
+import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--force", help="force check, even within 24h", action="store_true")
-parser.add_argument("-p", "--packages", help="create a new file with mentioned packages", nargs='+')
+group = parser.add_argument_group()
+parser.add_argument("--force", help="force check, even within 24h", action="store_true")
+parser.add_argument("-q", "--quiet", help="quied mode, no input asked.", action="store_true")
+group.add_argument("-f", "--file", help="create new package filename. Only specify the suffix and without extension.")
+group.add_argument("-p", "--package", help="packages to check", nargs='+')
 args = parser.parse_args()
 
+curtime=datetime.datetime.now()
+newpackagesfound=False
 ssl._create_default_https_context = ssl._create_unverified_context
 
-file="packages.json"
-curtime=datetime.datetime.now()
+if args.package and args.file==None:
+    print("No filename was specified with --file")
+    sys.exit(2)
 
-if args.packages:
-    checktime=curtime + datetime.timedelta(days=-1)
-    packagescur = {
-        "checktime": str(checktime),
-        "packages": []
-    }
-    for name in args.packages:
-        package = {
-            "name": name,
-            "version": 0
-        }
-        packagescur["packages"].append(package)
+if args.file:
+    files=["packages_" + args.file + ".json"]
 else:
-    try:
-        with open(file, "r") as f:
-            packagescur = json.load(f)
-        f.close()
-    except FileNotFoundError:
-        print(f"Error loading file {file}. Specify packages to create the file")
-        sys.exit(2)
-    except:
-        print(f"Unknown error occured:")
-        print(sys.exc_info())
-        sys.exit(2)
+    files=glob.glob("packages*.json")
+    if files == []:
+        print("No package files found. Please create one with --file and --package")
 
-checktime=datetime.datetime.fromisoformat(packagescur["checktime"])
-if args.force or (curtime-checktime).days > 0:
-    packagesnew = {
-        "checktime": str(datetime.datetime.now()),
-        "packages": []
-    }
-    for package in packagescur["packages"]:
-        link = "https://community.chocolatey.org/packages/" + package["name"]
-        try:
-            with urllib.request.urlopen(link) as response:
-                body = str(response.read())
-                
-                searchstr = '<td class="version"   title="Latest Version"  >'
-                index = body.find(searchstr)
-
-                buttonbegin = '<button '
-                indexbegin = body.rfind(buttonbegin,0,index)
-                buttonend = '>'
-                indexend = body.find(buttonend,indexbegin) + 1
-                buttonstr = body[indexbegin:indexend]
-
-                versionbegin = 'version="'
-                indexbegin = buttonstr.find(versionbegin) + len(versionbegin)
-                versionend = '"'
-                indexend = buttonstr.find(versionend,indexbegin)
-                version=buttonstr[indexbegin:indexend]
-        except urllib.error.HTTPError:
-            print(f"{package['name']} not found.")
-            version = package["version"]
-        except:
-            print(f"Unknown error occured fetching link { link }:")
-            print(sys.exc_info())
-            version = package["version"]
-
-        line = {
-            "name": package["name"],
-            "version": version
+for file in files:
+    if args.package:
+        checktime=curtime + datetime.timedelta(days=-1)
+        packagescur = {
+            "checktime": str(checktime),
+            "packages": []
         }
-        packagesnew["packages"].append(line)
-
-    if packagescur["packages"] != packagesnew["packages"]:
-        print("New package versions found.")
-
-    for i in range(len(packagesnew["packages"])):
-        if packagesnew["packages"][i] != packagescur["packages"][i]:
-            print(packagesnew["packages"][i])
-    with open(file, "w", encoding="utf-8") as f:
-        json.dump(packagesnew, f, ensure_ascii=False, indent=2)
-    f.close()
-
-    if packagescur["packages"] != packagesnew["packages"]:
-        input("Press ENTER to exit.")
+        for name in args.package:
+            package = {
+                "name": name,
+                "version": 0
+            }
+            packagescur["packages"].append(package)
     else:
-        print("No new packages found.")
-else:
-    print("Last check was within 24 hours ago. No need to check again.")
+        try:
+            with open(file, "r") as f:
+                packagescur = json.load(f)
+            f.close()
+        except FileNotFoundError:
+            print(f"Error loading file { file }. Specify --file and --package to create the file")
+            sys.exit(2)
+        except json.decoder.JSONDecodeError:
+            print(f"Skipping { file } as it is incorrect and can't be loaded.")
+            continue
+        except:
+            print(f"Unknown error occured:")
+            print(sys.exc_info())
+            sys.exit(2)
+
+    checktime=datetime.datetime.fromisoformat(packagescur["checktime"])
+    if args.force or (curtime-checktime).days > 0:
+        packagesnew = {
+            "checktime": str(datetime.datetime.now()),
+            "packages": []
+        }
+        for package in packagescur["packages"]:
+            link = "https://community.chocolatey.org/packages/" + package["name"]
+            try:
+                with urllib.request.urlopen(link) as response:
+                    body = str(response.read())
+                    
+                    searchstr = '<td class="version"   title="Latest Version"  >'
+                    index = body.find(searchstr)
+
+                    buttonbegin = '<button '
+                    indexbegin = body.rfind(buttonbegin,0,index)
+                    buttonend = '>'
+                    indexend = body.find(buttonend,indexbegin) + 1
+                    buttonstr = body[indexbegin:indexend]
+
+                    versionbegin = 'version="'
+                    indexbegin = buttonstr.find(versionbegin) + len(versionbegin)
+                    versionend = '"'
+                    indexend = buttonstr.find(versionend,indexbegin)
+                    version=buttonstr[indexbegin:indexend]
+            except urllib.error.HTTPError:
+                print(f"{package['name']} not found.")
+                version = package["version"]
+            except:
+                print(f"Unknown error occured fetching link { link }:")
+                print(sys.exc_info())
+                version = package["version"]
+
+            line = {
+                "name": package["name"],
+                "version": version
+            }
+            packagesnew["packages"].append(line)
+
+        if packagescur["packages"] != packagesnew["packages"]:
+            print(f"New package versions found for { file }.")
+            newpackagesfound=True
+        else:
+            print(f"No new packages found for { file }.")
+
+        for i in range(len(packagesnew["packages"])):
+            if packagesnew["packages"][i] != packagescur["packages"][i]:
+                print(packagesnew["packages"][i])
+        with open(file, "w", encoding="utf-8") as f:
+            json.dump(packagesnew, f, ensure_ascii=False, indent=2)
+        f.close()
+    else:
+        print(f"Last check for { file } was within 24 hours ago. No need to check again.")
+
+if args.quiet==False and newpackagesfound==True:
+    input("Press ENTER to exit.")
--- choco_upgrade.ps1
+++ choco_upgrade.ps1
@@ -1,6 +1,17 @@
-$Packages = Get-Content -Path .\packages.json | ConvertFrom-Json
-
-$Packages.packages | ForEach-Object {
-    $Package = $_
-    choco upgrade $Package.Name --version $Package.Version
+$File = Get-Item -Path "packages*.json"
+switch (($File | Measure-Object).Count){
+    0 {
+        Write-Output -InputObject "No pacakges-<nnn>.json file found."
+    }
+    1 {
+        $Packages = $File | Get-Content | ConvertFrom-Json
+        $Packages.packages | ForEach-Object {
+            $Package = $_
+            choco upgrade $Package.Name --version $Package.Version
+        }
+    }
+    default {
+        Write-Output -InputObject "Too many files found. Make sure only 1 packages-<nnn>.json exist."
+        Write-Output -InputObject $File.Name
+    }
 }
