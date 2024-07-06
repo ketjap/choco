4f4abb5385d2feb9d421824e3d7ead88e59d1e55
Sander Siemonsma <shrikes_number_0a@icloud.com>
2024-07-06 15:58:57 +0200

    only check > 24h ago

--- choco_fetchpackages.py
+++ choco_fetchpackages.py
@@ -8,10 +8,13 @@ import datetime
 ssl._create_default_https_context = ssl._create_unverified_context
 
 file="packages.json"
+curtime=datetime.datetime.now()
 
 if len(sys.argv) > 1:
+    checktime=curtime
+    checktime+=datetime.timedelta(days=-1)
     packagescur = {
-        "checktime": str(datetime.datetime.now()),
+        "checktime": str(checktime),
         "packages": []
     }
     for i in range(1,len(sys.argv)):
@@ -33,52 +36,59 @@ else:
         print(sys.exc_info())
         sys.exit(2)
 
-packagesnew = {
-    "checktime": str(datetime.datetime.now()),
-    "packages": []
-}
-for package in packagescur["packages"]:
-    link = "https://community.chocolatey.org/packages/" + package["name"]
-    try:
-        with urllib.request.urlopen(link) as response:
-            body = str(response.read())
-            
-            searchstr = '<td class="version"   title="Latest Version"  >'
-            index = body.find(searchstr)
-
-            buttonbegin = '<button '
-            indexbegin = body.rfind(buttonbegin,0,index)
-            buttonend = '>'
-            indexend = body.find(buttonend,indexbegin) + 1
-            buttonstr = body[indexbegin:indexend]
-
-            versionbegin = 'version="'
-            indexbegin = buttonstr.find(versionbegin) + len(versionbegin)
-            versionend = '"'
-            indexend = buttonstr.find(versionend,indexbegin)
-            version=buttonstr[indexbegin:indexend]
-    except urllib.error.HTTPError:
-        print(f"{package['name']} not found.")
-        version = package["version"]
-    except:
-        print(f"Unknown error occured fetching link { link }:")
-        print(sys.exc_info())
-        version = package["version"]
-
-    line = {
-        "name": package["name"],
-        "version": version
+checktime=datetime.datetime.fromisoformat(packagescur["checktime"])
+if (curtime-checktime).days > 0:
+    packagesnew = {
+        "checktime": str(datetime.datetime.now()),
+        "packages": []
     }
-    packagesnew["packages"].append(line)
+    for package in packagescur["packages"]:
+        link = "https://community.chocolatey.org/packages/" + package["name"]
+        try:
+            with urllib.request.urlopen(link) as response:
+                body = str(response.read())
+                
+                searchstr = '<td class="version"   title="Latest Version"  >'
+                index = body.find(searchstr)
 
-if packagescur["packages"] != packagesnew["packages"]:
-    print("New package versions found:")
+                buttonbegin = '<button '
+                indexbegin = body.rfind(buttonbegin,0,index)
+                buttonend = '>'
+                indexend = body.find(buttonend,indexbegin) + 1
+                buttonstr = body[indexbegin:indexend]
+
+                versionbegin = 'version="'
+                indexbegin = buttonstr.find(versionbegin) + len(versionbegin)
+                versionend = '"'
+                indexend = buttonstr.find(versionend,indexbegin)
+                version=buttonstr[indexbegin:indexend]
+        except urllib.error.HTTPError:
+            print(f"{package['name']} not found.")
+            version = package["version"]
+        except:
+            print(f"Unknown error occured fetching link { link }:")
+            print(sys.exc_info())
+            version = package["version"]
+
+        line = {
+            "name": package["name"],
+            "version": version
+        }
+        packagesnew["packages"].append(line)
+
+    if packagescur["packages"] != packagesnew["packages"]:
+        print("New package versions found.")
+
     for i in range(len(packagesnew["packages"])):
         if packagesnew["packages"][i] != packagescur["packages"][i]:
             print(packagesnew["packages"][i])
     with open(file, "w", encoding="utf-8") as f:
         json.dump(packagesnew, f, ensure_ascii=False, indent=2)
     f.close()
-    input("Press ENTER to exit.")
+
+    if packagescur["packages"] != packagesnew["packages"]:
+        input("Press ENTER to exit.")
+    else:
+        print("No new packages found.")
 else:
-    print("No new packages found.")
+    print("Last check was within 24 hours ago. No need to check again.")
